8869d2ed23b5322b7ca2e6c0a56ff3cf7faa08fd
jianguo <zjg555543@163.com>
2024-08-15 17:00:10 +0800

    update

--- rpcsyncer/rpc_syncer.go
+++ rpcsyncer/rpc_syncer.go
@@ -49,25 +49,24 @@ func (syncer *RPCSyncer) Start(ctx context.Context) {
 			if err != nil {
 				log.Fatal("error getting max batch: %v", err)
 			}
-			log.Infof("starting from batch %v, max batch %v", start, l2MaxBatch)
+			log.Infof("Starting from batch %v, max batch %v", start, l2MaxBatch)
 			if start > l2MaxBatch {
-				log.Infof("no new blocks to sync, sleep 10 seconds")
-				time.Sleep(10 * time.Second)
-				continue
-			}
-
-			if start > 10 {
-				start = start - 10
+				log.Infof("Sleep 30 seconds")
+				time.Sleep(30 * time.Second)
 			}
 
 			to := start + syncer.maxBatchSize - 1
-			if to > l2MaxBatch {
+			if to > l2MaxBatch && to > syncer.maxBatchSize {
 				to = l2MaxBatch
+				start = to - syncer.maxBatchSize + 1
 			}
+			log.Infof("Calling ..")
 			seqBatches, err := BatchesByNumbers(syncer.l2RPCUrl, start, to)
 			if err != nil {
 				log.Fatal("error getting batch data: %v", err)
 			}
+
+			log.Infof("Hashing ..")
 			offChainData := []types.OffChainData{}
 			for _, seqBatch := range seqBatches {
 				key := crypto.Keccak256Hash(seqBatch.BatchL2Data)
@@ -77,11 +76,12 @@ func (syncer *RPCSyncer) Start(ctx context.Context) {
 					Value: seqBatch.BatchL2Data,
 				})
 			}
+			log.Infof("Storing ..")
 			err = setStoreOffChainData(syncer.db, offChainData)
 			if err != nil {
 				log.Fatal("error storing off chain data: %v", err)
 			}
-			log.Infof("stored off chain data for batchs [%v,%v], size:%v", start, to, len(offChainData))
+			log.Infof("Stored data for batchs [%v,%v], size:%v", start, to, len(offChainData))
 			if setStartBlock(syncer.db, to) != nil {
 				log.Fatal("error setting start block: %v", err)
 			}
